7b78b0aa84dffe6922ce8bf4f99ab41daa41af04
Smbayi <mbayisoleil10@gmail.com>
2026-06-15 23:24:46 +0100

    premier commit

---
+++ script.js
@@ -0,0 +1,12 @@
+window.addEventListener("scroll", () => {
+
+    const nav = document.querySelector("nav");
+
+    if(window.scrollY > 50){
+        nav.style.background = "white";
+        nav.style.boxShadow = "0 2px 10px rgba(0,0,0,0.1)";
+    }else{
+        nav.style.boxShadow = "none";
+    }
+
+});
